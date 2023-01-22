f01149fb6ea158d4a664dab979a8dec63bd9632f
Andrei Chenchik <andrei@chenchik.me>
2023-01-22 15:27:00 +0100

    Init iOS13, iPhone only Xcode Project

---
+++ TrackerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackerApp
+//
+//  Created by Andrei Chenchik on 22/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
