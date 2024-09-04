529ba7a6deea6733fcd883c767ca9fb0d58a6ec7
Sergey Ivanov <ivanov.sergey.ekb@gmail.com>
2024-09-04 17:07:41 +0500

    init commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Sergey Ivanov on 04.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
